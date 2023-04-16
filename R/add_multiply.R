c0d96fed91710f55abd23d2f89d917dbfc496339
Matthew Gillies <mcgillies@shaw.ca>
2023-04-15 21:01:42 -0700

    added stuff

--- R/add_multiply.R
+++ R/add_multiply.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param a Number to be added after multiplication
+#' @param b First number to multiply
+#' @param c Second number to multiply
+#'
+#' @return A float
+#' @export
+#'
+#' @examples
+#' add_multiply(1,2,3)
 add_multiply <- function(a, b, c) {
   return (a + b * c)
 }
